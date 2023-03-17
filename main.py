66c719c6097a4f73ecdb273e30ba1a0346d53a79
GhanamKhalil <ghanamkhalil8@gmail.com>
2023-03-17 20:01:00 +0100

    script python pour etiendre ordinateur

---
+++ main.py
@@ -0,0 +1,50 @@
+import keyboard
+import time
+
+keyboard.press_and_release('win+r')
+
+
+time.sleep(1) # attender 1s pour terminer l'execution
+keyboard.write('powershell')
+keyboard.press_and_release('enter')
+time.sleep(2)
+
+
+
+keyboard.write('Stop-Computer')
+keyboard.press_and_release('enter')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# keyboard.write('dir')
+# keyboard.press_and_release('enter')
+# time.sleep(2)
+# keyboard.write('mkdir ghanamkhalil')
+# keyboard.press_and_release('enter')
+# time.sleep(1)
+# keyboard.write('cd ghanamkhalil')
+# keyboard.press_and_release('enter')
+# keyboard.write('echo GHANAM KHALIL IS HERE developpeur full stack Laravel React js 1. >> Ghanam.txt')
+# keyboard.press_and_release('enter')
+# keyboard.write('cls')
+# keyboard.press_and_release('enter')
+# keyboard.write('start Ghanam.txt')
+# # keyboard.write('rm ghanamkhalil')
+# keyboard.press_and_release('enter')
+
+
+
